3772e336b82418cd235759f38b40e82a4ec2c2f1
Austin <alcalza4@gmail.com>
2026-01-07 12:05:21 -0800

    removed cors

---
+++ README.md
@@ -0,0 +1,2 @@
+# Website
+The website was deployed to my own server, so you won't be able to see it, but all the backend pushes the files using the static dist.
--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
-app.use(cors())
 app.use(express.static('dist'))
 
 morgan.token('body', (req) => {
@@ -102,7 +100,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
